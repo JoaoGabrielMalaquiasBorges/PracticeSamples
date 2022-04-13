ab08d0a6dba0a84415e3b74fc0f020f1e597a5b7
jgmalaquias <joaomalaquiassb@gmail.com>
2022-04-12 22:04:44 -0300

    statically adds a fragment, makes its view clickable and attaches a click listener to it

---
+++ app/src/main/java/com/stellify/practicesamples/ClickableFragment.java
@@ -0,0 +1,28 @@
+package com.stellify.practicesamples;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
+
+public class ClickableFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View clickableView = inflater.inflate(R.layout.fragment_clickable,
+                                              container, false);
+        Toast toast = Toast.makeText(getContext(),
+                                     getResources().getString(R.string.click_message),
+                                     Toast.LENGTH_SHORT);
+
+        clickableView.setClickable(true);
+        clickableView.setOnClickListener(v -> toast.show());
+
+        return clickableView;
+    }
+}
